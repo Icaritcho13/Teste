e530e2fb5e8d1a738e6337d73801c8d5b93959e6
Ícaro Mac-Culloch <icaro.macculloch13@gmail.com>
2026-06-08 04:50:50 -0300

    Delete concorrencia-banco directory

--- concorrencia-banco/src/main/java/com/seuprojeto/concorrencia/exception/ContaNaoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.seuprojeto.concorrencia.exception;
-
-public class ContaNaoEncontradaException extends RuntimeException {
-    public ContaNaoEncontradaException(Long id) {
-        super("Conta não encontrada para o id: " + id);
-    }
-}
--- concorrencia-banco/src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
--- Conta da PARTE 1 (sem controle de concorrência). id = 1, saldo = 1000,00
-INSERT INTO conta_bancaria (saldo) VALUES (1000.00);
-
--- Conta da PARTE 2 (com @Version). id = 1, saldo = 1000,00, version = 0
--- O version = 0 é OBRIGATÓRIO: sem ele o primeiro UPDATE otimista compararia
--- contra NULL e o comportamento ficaria imprevisível.
-INSERT INTO conta_bancaria_versionada (saldo, version) VALUES (1000.00, 0);
